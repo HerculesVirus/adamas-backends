7d9f6a337764fbfa7a4440df8023287167cb2dce
ZainZakir <62980322+ZainZakir@users.noreply.github.com>
2023-03-02 18:13:08 +0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const frontAuth = require('../api/middleware/front/auth')
+const bearerToken = require('express-bearer-token')
+
+const app = express()
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({
+    limit: '50mb' , extended: true
+}))
+app.use(bearerToken())
+
